aac4ed2a80f191c46c91ba0cf02cbe9846f6680f
Benjamín Peña <benjamin@michelada.io>
2017-09-27 13:44:39 -0500

    Driver Model Validations

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,2 +1,7 @@
 class Driver < ApplicationRecord
+  validates :name, presence: true
+  validates :contact_num, presence: true
+  validates :driver_license, presence: true
+  validates :ine, presence: true
+  validates :address, presence: true
 end
